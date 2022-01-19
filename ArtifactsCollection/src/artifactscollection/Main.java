9e91e80886070afe3a6fdc22105e6acdb03363d5
George_Sakketos <sdi2000177@di.uoa.gr>
2022-01-19 16:43:22 +0200

    update

--- ArtifactsCollection/src/artifactscollection/Main.java
+++ ArtifactsCollection/src/artifactscollection/Main.java
@@ -4,26 +4,31 @@ public class Main
     public static void auction(Artifact[] array, String movement, String condition) 
     {
         int arrayLength = array.length;
-//        for (int arrayObject = 0; arrayObject < arrayLength; arrayObject++)
-//        {
-//            array[arrayObject].getIndex();
-//            array[arrayObject].getInfo();
-//            ((Painting)array[1]).evaluate(movement, condition);
-//        }
-
-        System.out.println(array[0].getClass().getSimpleName());
-        System.out.println(array[1].getClass().getSimpleName());
-        
-//        Class c = array[1].getClass();
-//        
-//        System.out.println(c.getSimpleName());
+        for (int arrayObject = 0; arrayObject < arrayLength; arrayObject++)
+        {
+            array[arrayObject].getIndex();
+            
+            boolean evaluationResult = false;
+            if ((array[arrayObject].getClass().getSimpleName()).equals("Painting"))
+            {
+                ((Painting)array[arrayObject]).getInfo();
+                evaluationResult = ((Painting)array[arrayObject]).evaluate(movement, condition);
+            }
+            else if ((array[arrayObject].getClass().getSimpleName()).equals("Sculpture"))
+            {
+                ((Sculpture)array[arrayObject]).getInfo();
+                evaluationResult = ((Sculpture)array[arrayObject]).evaluate(movement, condition);
+            }
+            
+            if (evaluationResult)
+                System.out.println("Evaluation: Successful");
+            else
+                System.out.println("Evaluation: Failier");
+        }
     }
     
     public static void main(String[] args) 
     {
-        Artifact[] objects = new Artifact[10];
-        objects[0] = new Masterpiece(1, "Mike", 1998, "move", "good");
-        objects[1] = new Painting(2, "Bob", 1958, "impressionism", "bad", 13, 10, "atechnique");
-        auction(objects, "impressionism", "bad");
+        
     }
 }
